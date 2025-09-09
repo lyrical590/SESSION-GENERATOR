e81f822b5928e0b81a108eef09f341978f69e409
lyrical590 <sammykibe31@gmail.com>
2025-09-09 03:45:03 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,13 +17,13 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *lyrical-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
+❒ *GitHub:* __https://github.com/lyrical590/LYRICAL-XMD
+❒ *Author:* _wa.me/254769799982_
 ❒ *YT:* _https://youtube.com/@wasitech10_
 └────────────⭓
 `
